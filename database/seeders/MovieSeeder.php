f15e797d97098652934caa2fc7fb40e894bbc9f3
Adam Pandey <N00234646@iadt.ie>
2024-10-21 14:14:45 +0100

    "Created Cards"

---
+++ database/seeders/MovieSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Movie;
+use Carbon\Carbon;
+
+class MovieSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $currentTimestamp = Carbon::now();
+        Movie::insert([
+            ['title'=> 'Transformers','release_date'=>'2007','image'=>'1.jpg',
+                'director'=>'Michael Bay'],
+            ['title'=> 'Transformers Revenge of the fallen','release_date'=>'2009','image'=>'2.jpg',
+                'director'=>'Michael Bay'],
+            ['title'=> 'Transformers Dark of The Moon','release_date'=>'2011','image'=>'3.jpg',
+                'director'=>'Michael Bay'],
+            ['title'=> 'Transformers Age Of Extinction','release_date'=>'2014','image'=>'4.jpg',
+                'director'=>'Michael Bay'],
+            ['title'=> 'Transformers The Last Knight','release_date'=>'2017','image'=>'5.jpg',
+                'director'=>'Michael Bay'],
+        ]);
+    }
+}
---
+++ resources/views/components/movie-card.blade.php
@@ -0,0 +1,8 @@
+@props(['title','release_date','image','director'])
+
+<div class="border rounded-lg shadow-md p-6 bg-white hover:shadow-lg transition duration-300">
+    <h4 class="font-bold text-lg">{{$title}}</h4>
+    <img src="{{asset('images/movies/'. $image)}}" alt="{{$title}}">
+    <p class="text-gray-600">({{$release_date}})</p>
+    <p class="text-gray-800 mt-4">{{$director}}</p>
+</div>
