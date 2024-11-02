03b904fdd1845bf0776aee729c4b8998afdbd9c5
N00234646@iadt.ie <N00234646@iadt.ie>
2024-11-02 15:01:40 +0000

    Working commit for edit form, errors still prevalent

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,27 +1,20 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+            {{__('Create New Movie') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h3 class="font-semibold text-lg mb-4">Edit New Movie:</h3>
+                    <x-movie-form
+                        :action="route('movies.update', $movie)"
+                        :method="'PUT'"
+                        :movie="$movie"
+                    />
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ Route::get('/movies',[MovieController::class,'index'])->name('movies.index');
 Route::get('/movies/create',[MovieController::class,'create'])->name('movies.create');
 Route::get('/movies/{movie}',[MovieController::class,'show'])->name('movies.show');
 Route::post('/movies',[MovieController::class,'store'])->name('movies.store');
+Route::get('/movies/edit',[MovieController::class,'edit'])->name('movies.edit');
+Route::get('/movies/destroy',[MovieController::class,'destroy'])->name('movies.destroy');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
