fc1ca53e7bd9d81a2af8e67b936402e603d8ee84
N00234646@iadt.ie <N00234646@iadt.ie>
2024-11-03 16:29:02 +0000

    Profile edit error ammended, no errors

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{_('All Transformers_movies')}}
+            {{_('All Transformers movies')}}
         </h2>
     </x-slot>
 
