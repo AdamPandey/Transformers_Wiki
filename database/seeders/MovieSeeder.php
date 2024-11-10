74198830c04562b9fb86306bc83e8c9a77aba895
N00234646@iadt.ie <N00234646@iadt.ie>
2024-11-10 16:56:40 +0000

    All errors fixed

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -27,7 +27,31 @@ class MovieSeeder extends Seeder
             ['title'=> 'Transformers The Last Knight','release_date'=>'2017','image'=>'5.jpg',
                 'director'=>'Michael Bay'],
             ['title'=> 'Transformers One','release_date'=>'2024','image'=>'6.jpg',
-            'director'=>'Josh Cooley']
+            'director'=>'Josh Cooley'],
+            ['title'=> 'Bumblebee','release_date'=>'2018','image'=>'7.jpg',
+            'director'=>'Travis Knight'],
+            ['title'=> 'Transformers Rise of The Beasts','release_date'=>'2023','image'=>'8.jpg',
+            'director'=>'Stephen Caple'],
+            ['title'=> 'Transformers Predacons Rising','release_date'=>'2013','image'=>'9.jpg',
+            'director'=>'Scooter Tidwell'],
+            ['title'=> 'The Transformers','release_date'=>'1984','image'=>'10.jpg',
+            'director'=>'Nelson Shinn'],
+            ['title'=> 'Beast Wars','release_date'=>'1996','image'=>'11.jpg',
+            'director'=>'Akira Nishimori'],
+            ['title'=> 'Beast Machines','release_date'=>'1999','image'=>'12.jpg',
+            'director'=>'Marty Isenberg'],
+            ['title'=> 'Robots in Disguise','release_date'=>'2000','image'=>'13.jpg',
+            'director'=>'Osamu Sekita'],
+            ['title'=> 'Armada','release_date'=>'2002','image'=>'14.jpg',
+            'director'=>'Hidehito Ueda'],
+            ['title'=> 'Energon','release_date'=>'2004','image'=>'15.jpg',
+            'director'=>'Eugene Ishikawa'],
+            ['title'=> 'Transformers Cybertron','release_date'=>'2005','image'=>'16.jpg',
+            'director'=>'Manabu Ono'],
+            ['title'=> 'Transformers Animated','release_date'=>'2007','image'=>'17.jpg',
+            'director'=>'Sam Register'],
+            ['title'=> 'Transformers Prime','release_date'=>'2010','image'=>'18.jpg',
+            'director'=>'Roberto Orci']
         ]);
     }
 }
